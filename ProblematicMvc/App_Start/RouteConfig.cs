f8c3966aa7fdc2f01183542f0a3706b21bf19f6f
Kaushal Kumar Panday <kaushalp@outlook.com>
2018-07-12 21:33:31 +0530

    Update RouteConfig.cs

--- ProblematicMvc/App_Start/RouteConfig.cs
+++ ProblematicMvc/App_Start/RouteConfig.cs
@@ -12,6 +12,8 @@ namespace ProblematicMvc
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            routes.IgnoreRoute("proxy/{*pathInfo}");  
+            routes.IgnoreRoute("google/{*pathInfo}");  
 
             routes.MapRoute(
                 name: "Default",
